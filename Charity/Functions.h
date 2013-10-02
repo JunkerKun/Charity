72ca1d4323a5d753a160a83589543546e79ae5cd
JunkerKun <seal92@rambler.ru>
2013-10-02 19:59:32 +0400

    Deleted all

--- Charity/Functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FUNCTIONS
-#define __FUNCTIONS
-
-#include <SFML/System.hpp>
-
-float Distance(int x1, int y1, int x2, int y2);
-float Increment(float start, float end, float speed);
-sf::Vector2f Interpolate2D(sf::Vector2f vec1, sf::Vector2f vec2, float scalar);
-#endif
